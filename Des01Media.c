e045fba4f85bf4f1f5fd0eaffd40d25ba494c089
David .C Gomes <52863043+MaeinesenieaM@users.noreply.github.com>
2023-08-29 11:03:04 -0300

    Add files via upload

---
+++ Des01Media.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main ()
+{
+	float nota, media, bim = 2;
+
+	setlocale (LC_ALL, "Portuguese");
+
+	printf ("#####Aprovador e Reprovador##### \n\n");
+	printf (" Digite o total de pontos obtidos: "); scanf("%f", &nota);
+
+    media = nota / 2;
+
+    if (media >= 7.0)
+    {
+    	printf ("\n \\\\Você foi aprovado!//\n");
+    }
+     else if (media < 4.0)
+    {
+    	printf ("\n //Sinto lhe disser que você foi reprovado.\\\\\n");
+    }
+     else
+    {
+    	printf ("\n No momento você esta em recuperação.\n");
+    }
+
+	printf ("\n\n    __o  bici! \n  _/><_ \n (_)/(_) \n  "); //BICI!!!
+	return 0;
+}
